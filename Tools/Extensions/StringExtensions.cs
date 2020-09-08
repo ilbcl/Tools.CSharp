ef5fc7d8ed19f6dc7359b757f4aed2c1083d4738
ilbcl <opstromae@gmail.com>
2020-09-08 22:58:00 +0300

    StringExtensions constructor

--- Tools/Extensions/StringExtensions.cs
+++ Tools/Extensions/StringExtensions.cs
@@ -21,6 +21,45 @@ namespace Tools.Extensions
 
         private static readonly Dictionary<char, char> KeyBoardChars;
 
+        static StringExtensions()
+        {
+            KeyBoardChars = new Dictionary<char, char>
+            {
+                {'q', 'й'},
+                {'w', 'ц'},
+                {'e', 'у'},
+                {'r', 'к'},
+                {'t', 'е'},
+                {'y', 'н'},
+                {'u', 'г'},
+                {'i', 'ш'},
+                {'o', 'щ'},
+                {'p', 'з'},
+                {'[', 'х'},
+                {']', 'ъ'},
+                {'a', 'ф'},
+                {'s', 'ы'},
+                {'d', 'в'},
+                {'f', 'а'},
+                {'g', 'п'},
+                {'h', 'р'},
+                {'j', 'о'},
+                {'k', 'л'},
+                {'l', 'д'},
+                {';', 'ж'},
+                {'\'', 'э'},
+                {'z', 'я'},
+                {'x', 'ч'},
+                {'c', 'с'},
+                {'v', 'м'},
+                {'b', 'и'},
+                {'n', 'т'},
+                {'m', 'ь'},
+                {',', 'б'},
+                {'.', 'ю'}
+            };
+        }
+
         public static string TruncateWithEllipsis(this string self, int maxLength)
         {
             if (null == self)
